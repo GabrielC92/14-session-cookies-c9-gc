f6dde03bf95b858ea6e28755cfefa657e3eb93e3
GabrielC92 <gabriel.calla2012@gmail.com>
2021-10-23 23:46:36 -0300

    validacion

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,7 +1,15 @@
+const {validationResult} = require('express-validator');
+
 module.exports = {
 	home: (req,res) => {
 		return res.render('index', {
 			title: 'Complete el formulario'
 		});
+	},
+	syc: (req,res) => {
+		let errors = validationResult(req);
+		if (errors.isEmpty()) {
+			
+		}
 	}
 }
--- src/validations/formValidator.js
+++ src/validations/formValidator.js
@@ -2,11 +2,19 @@ const {check} = require('express-validator');
 
 module.exports = [
     check('nombre')
-    .notEmpty().withMessage('Escriba su nombre'),
-
-    check('email')
-    .isEmail().withMessage('Escriba su email'),
+    .notEmpty().withMessage('Escriba su nombre').bail()
+    .isAlpha().withMessage('Usar solo caracteres alfábeticos'),
 
     check('color')
-    .notEmpty().withMessage('Escriba su email'),
+    .notEmpty().withMessage('Seleccione un color'),
+
+    check('email')
+    .notEmpty().withMessage('Escriba su email').bail()
+    .isEmail().withMessage('Debe usar un email válido'),
+
+    check('edad')
+    .isInt({
+        min: 18,
+        max: 99
+    }).withMessage('Escriba un número entre 18 y 99')
 ]
